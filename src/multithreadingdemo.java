a6484b6c25ca857666ba3dc6036383dcd5601b69
LOVISH066 <lovishgoyal066@gmail.com>
2026-01-21 20:41:28 +0530

    add

--- src/multithreadingdemo.java
+++ src/multithreadingdemo.java
@@ -2,6 +2,13 @@ public class multithreadingdemo {
     public static void main(String[] args) {
         
         // multithreading.
+         // executing severla tasks simulateno that are not independed t oeach other ieach process is independednt// runnning multiple tasks
+         // is claled process based multitasking // os level
+        // where is each task seearty by same pr
+        // executing sevelra task simultaely r parrely where is task speapreted independent part of same progaosd or process is caled thread basee dmutlitskign
+        // program level
+        
+        // thread based multitsking
         
     }
 }
---
+++ src/myThread.java
@@ -0,0 +1,9 @@
+class myThread extends Thread{
+
+    // we have to give thread a job or task
+    @Override // here we are using override becasue child clas sis not satify with parent class so we use overide\
+    public void run() {
+        
+        
+    }
+}
