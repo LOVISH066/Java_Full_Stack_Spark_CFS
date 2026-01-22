32e89e1c362105ed115334d4ba0624f0b8130a60
LOVISH066 <lovishgoyal066@gmail.com>
2026-01-22 19:45:40 +0530

    add

--- src/myThread.java
+++ src/myThread.java
@@ -3,7 +3,7 @@ class myThread extends Thread{
     // we have to give thread a job or task
     @Override // here we are using override becasue child clas sis not satify with parent class so we use overide\
     public void run() {
-        
+        // cpntext switvching 
         
     }
 }
