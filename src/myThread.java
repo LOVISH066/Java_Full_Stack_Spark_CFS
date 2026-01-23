00a1cfbdc944a3052d8ae99f67e50563103ef9a3
LOVISH066 <lovishgoyal066@gmail.com>
2026-01-23 16:13:01 +0530

    add

--- src/myThread.java
+++ src/myThread.java
@@ -4,6 +4,14 @@ class myThread extends Thread{
     @Override // here we are using override becasue child clas sis not satify with parent class so we use overide\
     public void run() {
         // cpntext switvching 
+
+        // tghread herechy usme main thread jo main method run karegya
+        // jo line rad karegya in main method that is main thread// t,start then you have tom ka enaothe rthread
+        // t.start then we say child of main thread
+        // thread shecduler
+        // ts is a compent tahtd ecideh which thread will work first
+        // t.srtart = new thread will be creatde, and run the task executing run method
+        
         
     }
 }
